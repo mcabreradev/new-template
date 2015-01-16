50843497da91eb1d26902d57f9c350af7d242593
Migue Cabrera <miguel4ngel@gmail.com>
2015-01-16 16:07:42 -0300

    edit gulpjs files

---
+++ gulp/server.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var gulp = require('gulp');
+
+var $ = require('gulp-load-plugins')({
+  pattern: ['gulp-*', 'main-bower-files', 'uglify-save-license', 'del']
+});
+
+var $bower = 'bower_components/',
+    $root = 'dist',
+    $port = 8888;
+
+function handleError(err) {
+  console.error(err.toString());
+  this.emit('end');
+}
+
+gulp.task('connect', function() {
+  $.connect.server({
+    root: $root,
+    port: $port,
+    livereload: true
+  });
+});
+
+gulp.task('markup', function () {
+  gulp.src('./src/*.html')
+    .pipe($.connect.reload());
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['./src/*.html'], ['markup']);
+});
+
+gulp.task('serve', ['connect', 'watch']);
